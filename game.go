15e7ff2b7ce84bed67eaa6e6685e6cd4474dae43
armen armen <armen.amirzadyan@gmail.com>
2019-02-22 23:25:02 +0300

    Adding_player0.2

--- game.go
+++ game.go
@@ -23,29 +23,30 @@ func Counting_of_players() {
 	var count_of_players int
 	fmt.Scan(&count_of_players)
 
+	var id_c int = count_of_players
+
 	if count_of_players > 1{
 		for i := 1;i <= count_of_players; {
-			for j := 1;j <= len(count_of_players);j++{
-				var count [j]int
-			}
-			Player_autorization(count)
+			Player_autorization(id_c)
 			i++
 		}
 	}else if count_of_players == 0 {
 		fmt.Println("not correct")
-		}else {Player_autorization()}
+		}else {
+			id_c = 1
+			Player_autorization(id_c)}
 }
 
-func Player_autorization(id) {
+func Player_autorization(id_p int) {
 	player := Player_data{}
 
-	for i := 1; i <= ;i++{
-		for i := 0; i < count; i++ {
-			fmt.Print("what's your name:")
-			fmt.Scan(&player.name)
+	for i := 1;i <= id_p;{
+		fmt.Print("what's your name:")
+		fmt.Scan(&player.name)
 
-			fmt.Print("How old are you:")
-			fmt.Scan(&player.age)
-		}
+		fmt.Print("How old are you:")
+		fmt.Scan(&player.age)
+		i++
+		fmt.Print(id_p," ", player.name," ", player.age)
 	}
 }
